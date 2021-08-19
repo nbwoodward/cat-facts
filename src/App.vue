8793b9f63bf4b057e71dad54c8bfc35f40943688
Nick Woodward <nick.woodward@onxmaps.com>
2021-08-19 13:06:33 -0600

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <CatFacts />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CatFacts from './components/CatFacts.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    CatFacts
   }
 }
 </script>
---
+++ src/components/CatFacts.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="cat-facts">
+    <h1> Cat Facts </h1>
+    <div> {{fact}} </div>
+    <button @click="setFact">Another please</button>
+  </div>
+</template>
+<script>
+import facts from '../facts'
+
+export default {
+  name: 'CatFacts',
+  data(){
+    return {
+      fact: ''
+    }
+  },
+  mounted(){
+    this.setFact()
+  },
+  methods: {
+    setFact(){
+      const rando = Math.floor(Math.random() * facts.length)
+      this.fact = facts[rando]
+    }
+  }
+}
+</script>
+<style scoped>
+button{
+  margin-top:15px;
+}
+</style>
